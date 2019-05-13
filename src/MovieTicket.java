797c0c205df5955608e70ee430291a892d90b7c6
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-13 09:36:45 +0200

    add new java class

---
+++ src/MovieTicket.java
@@ -0,0 +1,11 @@
+import java.util.GregorianCalendar;
+
+public class MovieTicket {
+
+    public double price;
+    public GregorianCalendar date;
+    public GregorianCalendar time;
+    public String movie;
+    public int cinemahall;
+
+}
