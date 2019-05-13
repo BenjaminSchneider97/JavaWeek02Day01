9c586ddec075a2ac37cbea4c6c47712f9f4bd0b4
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-13 15:17:13 +0200

    add seconds calculator

--- src/TimeClass.java
+++ src/TimeClass.java
@@ -26,6 +26,11 @@ public class TimeClass {
         this.second += addSecond;
     }
 
+    public void calcseconds(){
+        int seconds = this.hour * 3600 + this.minute * 60 + this.second;
+        System.out.println("The day is " + seconds + " seconds old!");
+    }
+
     public void printTime(){
         System.out.println(this.hour + ":" + this.minute + ":" + this.second);
     }
