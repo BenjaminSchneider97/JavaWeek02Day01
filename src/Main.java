71beb74af2410b9b65b9b88aae171fa681f9bd21
tjennyyy <jenny_tang@gmx.net>
2019-05-13 11:23:34 +0200

    //movies01

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /*
         Person person1 = new Person("Jenny", "Tang");
         Person person2 = new Person("Benjamin", "Schneider");
         Person person3 = new Person("Simon", "Blaha");
@@ -13,6 +14,19 @@ public class Main {
 
         for(int i = 0; i < fullnames.length; i++){
             fullnames[i].printFullName();
+        } */
+
+
+
+        movies m1 = new movies (8.9, "2019-05-13" ,"16:30" , "Avengers 4", 1);
+        movies m2 = new movies (9.9, "2019-05-14" ,"17:30" , "Avengers 3", 2);
+        movies m3 = new movies (7.9, "2019-05-15" ,"18:30" , "Avengers 2", 5);
+        movies m4 = new movies (6.9, "2019-05-13" ,"19:30" , "Avengers", 8);
+
+        movies[] allMovies = {m1,m2,m3,m4};
+        for ( int i = 0; i <allMovies.length; i++){
+            allMovies[i].getTotalPrice();
         }
+
     }
 }
